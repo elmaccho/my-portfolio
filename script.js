e40a885e7070fc8bb2ef56fabdc887d12dd53601
elmaccho <106253529+elmaccho@users.noreply.github.com>
2023-02-22 15:24:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const startTitle = document.querySelector('.startTitle')
 const startText = document.querySelector('.startText')
 const blinkEl = document.querySelector('.blink')
 
+const skills = document.querySelectorAll('.skill')
+const skillCounters = document.querySelectorAll('.skillCounter')
+const htmlBox = document.querySelector('.html')
+
+
 let TitleText = ["Cześć, jestem Maciej"]
 let TextText = ["Junior FrontEnd Developer"]
 let textPosition = 0
@@ -19,7 +24,6 @@ const openAnimation = () => {
         }, 700);
     }, 1500);
 }
-
 const OpenstartText = () => {
     startText.classList.add("startTextOpen")
     startText.textContent = TextText
@@ -31,5 +35,12 @@ const typewriter = () => {
         setTimeout(typewriter, 50)
     }
 }
-
 window.addEventListener('load', openAnimation)
+
+document.addEventListener('click', () => {
+    skills[0].classList.add('html')
+    skills[1].classList.add('css')
+    skills[2].classList.add('javascript')
+    skills[3].classList.add('php')
+    skills[4].classList.add('wordpress')
+})
